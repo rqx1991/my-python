34f3cd78adc51beea637a91617077d8bf3f7f03b
Always Mi <195511714@qq.com>
2018-02-19 22:50:38 +0800

    test - usage of dataFrame

--- testFrameData.py
+++ testFrameData.py
@@ -4,6 +4,7 @@
 # This file is used to test the usage of dataFrame.
 
 import pandas as pd
+import tushare as ts
 
 # There several ways to set freamData, this is one of the methods.
 originData = { 'code': [600600, 600601],
@@ -94,6 +95,25 @@ if queryData.empty:
 # 600601   12
 # 600602   16
 
+joinArray = { 'code': [600602, 600603],
+                'roe-join': [15, 16]}
+joinData = pd.DataFrame(data = joinArray).set_index('code')
+crt_data = indexData.join(joinData,how='outer')
+print(crt_data)
+#          roe  roe-join
+# code
+# 600600  10.0       NaN
+# 600601  12.0       NaN
+# 600602   NaN      15.0
+# 600603   NaN      16.0
+
+addPrefixData = indexData.add_prefix(str(year*100+quarter)+'-')
+print(addPrefixData)
+#         201703-roe
+# code
+# 600600          10
+# 600601          12
+
 print('\n\n-------------test begin here----------------\n\n')
 
 
@@ -125,15 +145,19 @@ print('\n\n-------------test begin here----------------\n\n')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 print('\n\n-------------test end here----------------\n\n')
+
+# define a function that would use another function as parameter
+def func1(n):
+    print(str(n))
+def func2(n):
+    print('func2'+str(n))
+def mainFunc(funcName, str):
+    funcName(str)
+mainFunc(func2, '---str')
+# func2---str
+
+def myFunc(funcName):
+    print(funcName())
+func = ts.get_stock_basics
+# myFunc(func)
